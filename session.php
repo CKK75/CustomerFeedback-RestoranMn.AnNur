493361051b83d8b59bb58cf83604dd0575930816
CKK75 <147610757+CKK75@users.noreply.github.com>
2023-10-24 17:13:35 +0800

    Update session.php

--- session.php
+++ session.php
@@ -5,7 +5,7 @@ session_start();
 
 // if the user is already logged in then redirect user to welcome page
 if (isset($_SESSION["userid"]) && $_SESSION["userid"] == true){
-    header("location: Welcome Page.html");
+    header("location: ./Welcome Page.html");
     exit;
 }else{
     /*echo " Unable to redirect to Welcome page."*/;
@@ -18,7 +18,7 @@ if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 
     // Session has timed out
     session_unset();                            // Unset all session variables
     session_destroy();                          // Destroy the session
-    header("Location: logout.php");             // Redirect to a logout page
+    header("Location: ./logout.php");             // Redirect to a logout page
     exit;
 }else{
     // Update the last activity time
