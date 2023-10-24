dede6d3068f844bfab8173b0e5de2332de85c466
CKK75 <147610757+CKK75@users.noreply.github.com>
2023-10-24 17:14:17 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is not logged in, then redirect the user to login page
 if (!isset($_SESSION["userid"]) || $_SESSION["userid"] !== true) {
-    header("location: login.php");
+    header("location: ./login.php");
     exit;
 }
 
